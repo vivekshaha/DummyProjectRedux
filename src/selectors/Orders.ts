8b883a6de760acb8421ec8c8d2f09cf78402b1de
Vivek Kumar <97436932+vivekshaha@users.noreply.github.com>
2023-05-03 13:23:09 +0530

    optimization using create selectore

--- src/selectors/Orders.ts
+++ src/selectors/Orders.ts
@@ -1,27 +1,61 @@
 import { product } from "../models/Products";
 import { State } from "../store";
+import { createSelector } from "reselect";
 
-export const orderloadingSelctor = (state: State) => {
-  return state.orders.loading;
-};
-export const ordersSelctor = (state: State) => {
-  const NormalizeOrders = state.orders.Orders;
+export const StateOrderSelector = (state: State) => state.orders;
+export const OrderProductSelector = (state: State) => state.product.products;
+
+export const orderloadingSelctor = createSelector(
+  StateOrderSelector,
+  (StateOrder) => StateOrder.loading
+);
+
+// export const orderloadingSelctor = (state: State) => {
+//   return state.orders.loading;
+// };
+
+export const ordersMapSelctor = createSelector(
+  StateOrderSelector,
+  (StateOrder) => StateOrder.Orders
+);
+
+// export const ordersMapSelctor = (state: State) => {
+//   return state.orders.Orders;
+// };
+export const ordersSelctor = createSelector(ordersMapSelctor, (StateOrders) => {
+  const NormalizeOrders = StateOrders;
   const orders = Object.keys(NormalizeOrders).map((k) => NormalizeOrders[+k]);
-
   return orders;
-};
-export const ordersMapSelctor = (state: State) => {
-  return state.orders.Orders;
-};
+});
 
-export const orderProductSelector = (state: State) => {
-  return Object.keys(state.orders.Orders).reduce<{
-    [orderId: number]: product[];
-  }>((prev, currentId) => {
-    const order = state.orders.Orders[+currentId];
-    const products = order.products.map((pid) => {
-      return state.product.products[pid];
-    });
-    return { ...prev, [currentId]: products };
-  }, {});
-};
+// export const ordersSelctor = (state: State) => {
+//   const NormalizeOrders = state.orders.Orders;
+//   const orders = Object.keys(NormalizeOrders).map((k) => NormalizeOrders[+k]);
+//   return orders;
+// };
+
+export const orderProductSelector = createSelector(
+  ordersMapSelctor,
+  OrderProductSelector,
+  (StateOrders, OrderProdcuts) =>
+    Object.keys(StateOrders).reduce<{
+      [orderId: number]: product[];
+    }>((prev, currentId) => {
+      const order = StateOrders[+currentId];
+      const products = order.products.map((pid) => {
+        return OrderProdcuts[pid];
+      });
+      return { ...prev, [currentId]: products };
+    }, {})
+);
+// export const orderProductSelector = (state: State) => {
+//   return Object.keys(state.orders.Orders).reduce<{
+//     [orderId: number]: product[];
+//   }>((prev, currentId) => {
+//     const order = state.orders.Orders[+currentId];
+//     const products = order.products.map((pid) => {
+//       return state.product.products[pid];
+//     });
+//     return { ...prev, [currentId]: products };
+//   }, {});
+// };
--- src/selectors/Products.ts
+++ src/selectors/Products.ts
@@ -1,12 +1,30 @@
+import { createSelector } from "reselect";
 import { State } from "../store";
 
-export const poroductloadingslector = (state: State) => {
-  return state.product.loading;
-};
-export const poroductloadedselector = (state: State) => {
-  const normalizeProducts = state.product.products;
-  const product = Object.keys(normalizeProducts).map(
-    (k) => normalizeProducts[+k]
-  );
-  return product;
-};
+export const ProductSelctor = (state: State) => state.product;
+
+export const poroductloadingslector = createSelector(
+  ProductSelctor,
+  (StateProduct) => StateProduct.loading
+);
+
+// export const poroductloadingslector = (state: State) => {
+//   return state.product.loading;
+// };
+export const poroductloadedselector = createSelector(
+  ProductSelctor,
+  (StateProduct) => {
+    const normalizeProducts = StateProduct.products;
+    const product = Object.keys(normalizeProducts).map(
+      (k) => normalizeProducts[+k]
+    );
+    return product;
+  }
+);
+// export const poroductloadedselector = (state: State) => {
+//   const normalizeProducts = state.product.products;
+//   const product = Object.keys(normalizeProducts).map(
+//     (k) => normalizeProducts[+k]
+//   );
+//   return product;
+// };
